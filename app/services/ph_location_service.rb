ea52931a8942e1b2491ab67275240f298e819045
plok74122 <plok74122@gmail.com>
2022-11-11 11:25:16 +0800

    done fetch provincs

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -14,4 +14,16 @@ class PhLocationService
       address_region.save
     end
   end
+
+  def fetch_province
+    request = RestClient.get("#{url}/provinces")
+    data = JSON.parse(request.body)
+    data.each do |province|
+      address_province = Address::Province.find_or_initialize_by(code: province['code'])
+      region = Address::Region.find_by_code(province['regionCode'])
+      address_province.region = region
+      address_province.name = province['name']
+      address_province.save
+    end
+  end
 end

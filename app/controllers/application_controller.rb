bf4a5b9cef59c57516da72d8b327d03e10621a6b
plok74122 <plok74122@gmail.com>
2022-11-17 10:57:54 +0800

    adjust after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource) if is_navigational_format?
   end
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admin_users_path
+    else
+      posts_path
+    end
+  end
 end

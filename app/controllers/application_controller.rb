362ea062f6870576514e61a570e3564d474f767a
plok74122 <plok74122@gmail.com>
2022-11-17 11:46:52 +0800

    language switcher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,13 @@ class ApplicationController < ActionController::Base
       posts_path
     end
   end
+
+  before_action :set_locale
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,8 @@ module App
     #
     config.time_zone = "Hong Kong"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.i18n.default_locale = "en"
+    config.i18n.available_locales = [:en, 'zh-CN']
     # Don't generate system test files.
     config.generators.system_tests = nil
   end

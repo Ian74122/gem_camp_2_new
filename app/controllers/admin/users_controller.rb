6ad5269e5e7b54f99f1fbe18719ab4ca15e347a9
plok74122 <plok74122@gmail.com>
2022-11-17 10:38:13 +0800

    add config for test domain check admin in back stage

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,12 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_admin
+
   def index
     @users = User.all
+  end
+
+  def check_admin
+    raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
   end
 end

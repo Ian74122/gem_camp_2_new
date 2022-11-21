74ab3900fff35260565415af1ee8b9328796bfac
plok74122 <plok74122@gmail.com>
2022-11-21 10:36:52 +0800

    gem install pundit and set up config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource) if is_navigational_format?
   end
@@ -11,12 +15,24 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  before_action :set_locale
   def set_locale
     if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
       session[:locale] = params[:locale]
     end
 
     I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: 'pundit', default: :default,
+                      username: exception.policy.record&.user&.email
+    redirect_to(request.referrer || root_path)
+  end
+
+  def pundit_user
+    current_user
   end
 end

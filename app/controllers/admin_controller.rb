6d942bbc854155a6ba6bb42c1c0aff296d9f672f
plok74122 <plok74122@gmail.com>
2022-11-21 11:17:14 +0800

    add config to admin controller , for pundit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,21 @@
 class AdminController < ActionController::Base
   before_action :check_admin
   before_action :authenticate_user!
-  
+  include Pundit
   def check_admin
     raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
+  end
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: 'pundit', default: :default,
+                      username: exception.policy.record&.user&.email
+    redirect_to(request.referrer || root_path)
+  end
+
+  def pundit_user
+    current_user
   end
 end

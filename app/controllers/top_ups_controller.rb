8a8840608fa78612d4326562a8cc8f98d6167695
plok74122 <plok74122@gmail.com>
2022-11-22 12:03:03 +0800

    add comment what to do

--- app/controllers/top_ups_controller.rb
+++ app/controllers/top_ups_controller.rb
@@ -9,5 +9,9 @@ class TopUpsController < ApplicationController
     @order.amount = params[:order][:amount]
     @order.user = current_user
     @order.save
+    # call third party get a url
+    # build callback api
+    # start your server by ngrok
+    # waiting server callback (info bernard & tell serial number to paid)
   end
 end

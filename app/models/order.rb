d8abd27160d62bbf399b609f23e49d62aa489c06
plok74122 <plok74122@gmail.com>
2022-11-22 11:03:33 +0800

    design aasm state and event

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,25 @@
 class Order < ApplicationRecord
   include AASM
   belongs_to :user
+
+  aasm column: :state do
+    state :pending, initial: true
+    state :submitted, :paid, :failed, :revoked
+
+    event :submit do
+      transitions from: :pending, to: :submitted
+    end
+
+    event :pay do
+      transitions from: :submitted, to: :paid
+    end
+
+    event :fail do
+      transitions from: [:pending, :submitted], to: :failed
+    end
+
+    event :revoke do
+      transitions from: [:pending, :submitted], to: :revoked
+    end
+  end
 end
